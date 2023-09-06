f41ba628f8c7000f30e77668bcfec2c39d3aed55
S-Keyp <s-keyp@outlook.fr>
2023-09-06 11:10:57 +0200

    feat: score pipes + pseudo to uppercase

---
+++ src/app/shared/pipes/score.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'score'
+})
+export class ScorePipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): unknown {
+      if(value >= 0) return '+' + value
+      return value;
+    
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LikeHateComponent } from '../shared/components/like-hate/like-hate.component';
 import { ColleagueComponent } from './components/colleague/colleague.component';
 import { ColleagueListComponent } from '../shared/components/colleague-list/colleague-list.component';
+import { ScorePipe } from '../shared/pipes/score.pipe';
 
 
 @NgModule({
   declarations: [
     LikeHateComponent,
     ColleagueComponent,
-    ColleagueListComponent
+    ColleagueListComponent,
+    ScorePipe
   ],
   imports: [
     CommonModule
